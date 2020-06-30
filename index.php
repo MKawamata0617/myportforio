cd6c7bf40aae33287eeaf5fd5eecaa4818235393
MKawamata0617 <58576295+MKawamata0617@users.noreply.github.com>
2020-06-30 15:24:17 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>contact</title>
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">contact</div>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form method="post" action="sent.php">
+        <div class="form-item">名前</div>
+        <input type="text" name="name">
+
+        <div class="form-item">年齢</div>
+        <select name="age">
+          <option value="未選択">選択してください</option>
+          <?php 
+            for ($i = 1; $i <= 100; $i++) {
+              echo "<option value='{$i}'>{$i}</option>";
+            } 
+          ?>
+        </select>
+
+        <div class="form-item">お問い合わせの種類</div>
+        <?php 
+          $types = array('質問', 'ポートフォリオに対する意見', '仕事に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+         ?>
+        <select name="category">
+          <option value="未選択">選択してください</option>
+           <?php
+             foreach ($types as $type) {
+              echo "<option value='{$type}'>{$type}</option>";
+             }
+           ?>
+
+        </select>
+
+        <div class="form-item">内容</div>
+        <textarea name="body"></textarea>
+
+        <input type="submit" value="送信">
+      </form>
+    </div>
+  </div>
+</body>
+</html>
