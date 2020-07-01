a9f27763e7b17e586c546529c5a71daa912b1d6c
MKawamata0617 <58576295+MKawamata0617@users.noreply.github.com>
2020-07-01 14:14:36 +0900

    Create sent.php

---
+++ sent.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>contact</title>
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">contact</div>
+  </div>
+
+  <div class="main">
+    <div class="thanks-message">お問い合わせいただきありがとうございます。</div>
+    <div class="display-contact">
+      <div class="form-title">入力内容</div>
+
+      <div class="form-item">■ 名前</div>
+      <?php echo $_POST['name']; ?>
+
+      <div class="form-item">■ 年齢</div>
+      <?php echo $_POST['age']; ?>
+
+      <div class="form-item">■ お問い合わせの種類</div>
+      <?php echo $_POST['category']; ?>
+
+      <div class="form-item">■ 内容</div>
+      <?php echo $_POST['body']; ?>
+
+    </div>
+  </div>
+</body>
+</html>
